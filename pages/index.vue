b9d7093e9b302f829f2634140d0e8ad125f0813b
ydah <13041216+ydah@users.noreply.github.com>
2022-01-12 01:55:19 +0900

    各ページへのリンクを追加する

--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,23 @@
         <p class="text-body-2">
           I'm also personal working as a culinary researcher and photographer.
         </p>
+        <v-divider class="my-5" />
+        <NuxtLink to="/blog" class="mr-5">Blog</NuxtLink>
+        <NuxtLink to="/work/cook" class="mr-5">Cooking</NuxtLink>
+        <NuxtLink to="/work/photo" class="mr-5">Photograph</NuxtLink>
+        <NuxtLink to="/contact" class="mr-5">Contact</NuxtLink>
+        <v-btn
+          icon
+          @click="linkToOtherWindow('https://www.instagram.com/ydah__/')"
+        >
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+        <v-divider class="my-5" />
+        <v-img
+          class="image-link"
+          src="https://grass-graph.appspot.com/images/ydah.png?background=none"
+          @click="linkToOtherWindow('https://github.com/ydah')"
+        />
       </v-col>
       <v-col md="6" sm="12">
         <v-img src="/lotus.jpg" contain />
@@ -28,6 +45,11 @@
 <script>
 export default {
   name: 'IndexPage',
+  methods: {
+    linkToOtherWindow(url) {
+      window.open(url, '_blank')
+    },
+  },
 }
 </script>
 
@@ -37,5 +59,9 @@ h1.title {
   -moz-osx-font-smoothing: grayscale;
   font-family: Audrey, sans-serif !important;
   font-weight: 100;
+}
+
+.image-link {
+  cursor: pointer;
 }
 </style>
