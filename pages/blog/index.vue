d62471c354fc32e624233fcfebe9931100eb2c99
ydah <13041216+ydah@users.noreply.github.com>
2022-01-12 00:39:54 +0900

    blogをydah-blogから移植する

---
+++ pages/blog/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="post in posts" :key="post.slug">
+        <v-card class="mx-auto" max-width="344" outlined>
+          <nuxt-link :to="'/blog/posts/' + post.slug">
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">
+                  {{ $dateFns.format(new Date(post.createdAt), 'yyyy/MM/dd') }}
+                </div>
+                <v-list-item-title class="text-h5 mb-1">{{
+                  post.title
+                }}</v-list-item-title>
+                <v-list-item-subtitle>{{
+                  post.description
+                }}</v-list-item-subtitle>
+                <v-list-item-subtitle>Tag</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-avatar tile size="80" color="grey" />
+            </v-list-item>
+          </nuxt-link>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({ $content }) {
+    const posts = await $content('posts').sortBy('createdAt', 'desc').fetch()
+    return { posts }
+  },
+}
+</script>
---
+++ pages/blog/posts/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <h1>{{ post.title }}</h1>
+    <v-divider />
+    <NuxtContent :document="post" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const post = await $content('posts', params.slug).fetch()
+    return { post }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+article {
+  font-family: 'Roboto', 'M PLUS 1p', sans-serif;
+}
+</style>
