7a41d5477f708e03fcd71111e3fb43491aba6064
ydah <13041216+ydah@users.noreply.github.com>
2022-01-12 02:16:02 +0900

    ブログ用のレイアウトを追加する

---
+++ layouts/blog.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-app-bar color="white" elevation="0" class="text-center"
+        ><v-spacer />
+        <h1 class="page__title">ydah blog.</h1>
+        <v-spacer
+      /></v-app-bar>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer color="white" elevation="0" absolute app>
+      <v-spacer />
+      <span class="footer__text"
+        >Copyright &copy; {{ new Date().getFullYear() }} - ydah</span
+      >
+      <v-spacer />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'BlogLayout',
+  data() {
+    return {
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+h1.page__title,
+.footer__text {
+  font-family: 'AntroVectra';
+  font-weight: 300;
+}
+</style>
