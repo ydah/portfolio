adc534fbe4011b19b00c71fa87ed22eb232da038
ydah <13041216+ydah@users.noreply.github.com>
2022-05-14 07:39:52 +0900

    Fix layout

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,33 +1,20 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col v-for="post in posts" :key="post.slug">
-        <v-card class="mx-auto" max-width="344" outlined>
-          <nuxt-link :to="'/blog/posts/' + post.slug" class="card-link">
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="text-overline mb-4">
-                  {{ $dateFns.format(new Date(post.published), 'yyyy/MM/dd') }}
-                </div>
-                <v-list-item-title class="text-h5 mb-1">{{
-                  post.title
-                }}</v-list-item-title>
-                <v-list-item-subtitle>{{
-                  post.description
-                }}</v-list-item-subtitle>
-                <v-list-item-subtitle></v-list-item-subtitle>
-              </v-list-item-content>
-              <v-list-item-avatar tile size="80" color="grey"
-                ><img
-                  v-if="post.eye_catch"
-                  :src="post.eye_catch"
-                  alt="eye catch"
-              /></v-list-item-avatar>
-            </v-list-item>
-          </nuxt-link>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-list two-line>
+      <v-list-item v-for="post in posts" :key="post.index">
+        <nuxt-link :to="'/blog/posts/' + post.slug" class="card-link">
+          <v-list-item-content>
+            <div class="text-overline mb-0">
+              {{ $dateFns.format(new Date(post.published), 'yyyy/MM/dd') }}
+            </div>
+            <v-list-item-title class="text-h5 mb-1">{{
+              post.title
+            }}</v-list-item-title>
+            <v-list-item-subtitle>{{ post.tag }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </nuxt-link>
+      </v-list-item>
+    </v-list>
   </v-container>
 </template>
 
